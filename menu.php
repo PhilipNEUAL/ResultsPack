863a82322aee765b38748679b5e81a7d7cf79673
PhilipNEUAL <neualtournaments@gmail.com>
2026-09-18 21:13:46 +0100

    prepare tempest tools for field test

--- menu.php
+++ menu.php
@@ -1,19 +1,38 @@
 <?php
-if (!isset($ret['RESULTSPACK'])) {
-    $ret['RESULTSPACK'][] = 'Results Pack';
+
+if (!isset($ret['RESULTSPACK']) || !is_array($ret['RESULTSPACK'])) {
+    $ret['RESULTSPACK'] = array();
 }
 
-$ret['RESULTSPACK'][] =
+$resultsPackTitle = 'Results Pack';
+
+$buildResults =
     'Build results sheet|'
     . $CFG->ROOT_DIR
     . 'Modules/Custom/ResultsPack/';
 
-$ret['RESULTSPACK'][] =
-    'Weather monitor / session control|'
+$weatherMonitor =
+    'TWIT|'
     . $CFG->ROOT_DIR
     . 'Modules/Custom/ResultsPack/TempestTest.php';
 
-$ret['RESULTSPACK'][] =
+$weatherViewer =
     'Weather session viewer|'
     . $CFG->ROOT_DIR
     . 'Modules/Custom/ResultsPack/WeatherSessionView.php';
+
+if (!in_array($resultsPackTitle, $ret['RESULTSPACK'], true)) {
+    array_unshift($ret['RESULTSPACK'], $resultsPackTitle);
+}
+
+if (!in_array($buildResults, $ret['RESULTSPACK'], true)) {
+    $ret['RESULTSPACK'][] = $buildResults;
+}
+
+if (!in_array($weatherMonitor, $ret['RESULTSPACK'], true)) {
+    $ret['RESULTSPACK'][] = $weatherMonitor;
+}
+
+if (!in_array($weatherViewer, $ret['RESULTSPACK'], true)) {
+    $ret['RESULTSPACK'][] = $weatherViewer;
+}
